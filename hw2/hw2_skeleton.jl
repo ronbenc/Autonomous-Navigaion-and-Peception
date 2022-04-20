2f661dd347dca9e5995ba4861d43304d4bcb2b83
Ron Benchetrit <ronbenc@campus.technion.ac.il>
2022-04-20 14:54:35 +0300

    Split Main

--- hw2/hw2_skeleton.jl
+++ hw2/hw2_skeleton.jl
@@ -35,6 +35,9 @@ function PropagateUpdateBelief(b::FullNormal, 𝒫::POMDPscenario, a::Array{Floa
     Σw, Σv = 𝒫.Σw, 𝒫.Σv
     # predict
     mv = PropagateBelief(b,𝒫,a)
+    if(o === nothing) # no observation
+        return mv
+    end
     μp = mv.μ
     Σp = mv.Σ
     # update
@@ -59,25 +62,24 @@ end
 # ron - a helper function for 2.a
 function GenerateRelativeObservation(x::Array{Float64, 1}, x_b::Array{Float64, 1}, r::Float64, rmin::Float64, )::Array{Float64, 1}
     Σv = 0.01*max(r, rmin)*[1.0 0.0; 0.0 1.0]
-    rel_loc = x_b - x
+    rel_loc = x
     noise = rand(MvNormal([0.0, 0.0], Σv)) # generate white noise with covariance Σv and zero mean
     return rel_loc + noise
 end
 
 
 function GenerateObservationFromBeacons(𝒫::POMDPscenario, x::Array{Float64, 1})::Union{NamedTuple, Nothing}
-    distances = [norm(x - 𝒫.beacons[:, i]) for i in range(1, length=size(𝒫.beacons, 2))]
+    distances = [norm(x - 𝒫.beacons[i, :]) for i in range(1, length=size(𝒫.beacons, 1))]
     for (index, distance) in enumerate(distances)
         if distance <= 𝒫.d
-            obs = GenerateRelativeObservation(x, 𝒫.beacons[:, index], distance, 𝒫.rmin)
+            obs = GenerateRelativeObservation(x, 𝒫.beacons[index, :], distance, 𝒫.rmin)
             return (obs=obs, index=index) 
         end    
     end 
     return nothing    
 end    
 
-
-function main()
+function part1()
     # definition of the random number generator with seed 
     rng = MersenneTwister(1)
     μ0 = [0.0,0.0]
@@ -85,18 +87,20 @@ function main()
     b0 = MvNormal(μ0, Σ0)
     d =1.0 
     rmin = 0.1
+
     # set beacons locations 
-    beacons =  transpose([0 0; 0 4; 0 8; 4 0; 4 4; 8 4; 0 8; 4 8; 8 8])
+    beacons =  [0.0 0.0; 0.0 4.0; 0.0 8.0; 4.0 0.0; 4.0 4.0; 4.0 8.0; 8.0 0.0; 8.0 4.0; 8.0 8.0]
+
     𝒫 = POMDPscenario(F=[1.0 0.0; 0.0 1.0],
-                      Σw=0.1^2*[1.0 0.0; 0.0 1.0],
-                      Σv=0.01^2*[1.0 0.0; 0.0 1.0], 
-                      rng = rng , beacons=beacons, d=d, rmin=rmin) 
+                    Σw=0.1^2*[1.0 0.0; 0.0 1.0],
+                    Σv=0.01^2*[1.0 0.0; 0.0 1.0], 
+                    rng = rng , beacons=beacons, d=d, rmin=rmin) 
     ak = [1.0, 0.0]
     xgt0 = [0.5, 0.2]
     T = 10
     # generating the trajectory
     τ = [xgt0]
-    
+
     # generate motion trajectory
     for i in 1:T-1
         push!(τ, SampleMotionModel(𝒫, ak, τ[end]))
@@ -106,22 +110,20 @@ function main()
     for i in 1:T
         push!(τobs, GenerateObservation(𝒫, τ[i]))
     end  
-    
+
     # generate beliefs dead reckoning 
     τbp = [b0]
-    
+
     for i in 1:T-1
         push!(τbp, PropagateBelief(τbp[end],  𝒫, ak))
     end
-    
+
     #generate posteriors 
     τb = [b0]
     for i in 1:T-1
         push!(τb, PropagateUpdateBelief(τb[end],  𝒫, ak, τobs[i+1]))
     end
 
-
-    
     # plots 
     dr=scatter([x[1] for x in τ], [x[2] for x in τ], label="gt")
     for i in 1:T
@@ -134,11 +136,33 @@ function main()
         covellipse!(τb[i].μ, τb[i].Σ, showaxes=true, n_std=3, label="step $i")
     end
     savefig(tr,"tr.pdf")
+end
 
-    
-               
-    xgt0 = [-0.5, -0.2]           
-    ak = [0.1, 0.1]           
+function part2()
+    # definition of the random number generator with seed 
+    rng = MersenneTwister(1)
+    μ0 = [0.0,0.0]
+    Σ0 = [1.0 0.0; 0.0 1.0]
+    b0 = MvNormal(μ0, Σ0)
+    d = 1.0 
+    rmin = 0.1
+
+    # set beacons locations 
+    beacons =  [0.0 0.0; 0.0 4.0; 0.0 8.0; 4.0 0.0; 4.0 4.0; 4.0 8.0; 8.0 0.0; 8.0 4.0; 8.0 8.0]
+
+    bplot =  scatter(beacons[:, 1], beacons[:, 2], label="beacons", markershape=:utriangle)
+    savefig(bplot,"beacons.pdf")
+
+    𝒫 = POMDPscenario(F=[1.0 0.0; 0.0 1.0],
+                    Σw=0.1^2*[1.0 0.0; 0.0 1.0],
+                    Σv=0.01^2*[1.0 0.0; 0.0 1.0], 
+                    rng = rng , beacons=beacons, d=d, rmin=rmin)
+
+    ak = [0.1, 0.1]
+    xgt0 = [-0.5, -0.2]
+    T = 100
+    # generating the trajectory
+    τ = [xgt0]
 
     # generate motion trajectory
     for i in 1:T-1
@@ -149,12 +173,44 @@ function main()
     τobsbeacons = []
     for i in 1:T
         push!(τobsbeacons, GenerateObservationFromBeacons(𝒫, τ[i]))
-    end  
+    end 
 
     println(τobsbeacons)
-    bplot =  scatter(beacons[:, 1], beacons[:, 2], label="beacons", markershape=:utriangle)
-    savefig(bplot,"beacons.pdf")
 
+    # generate beliefs dead reckoning 
+    τbp = [b0]
+
+    for i in 1:T-1
+        push!(τbp, PropagateBelief(τbp[end],  𝒫, ak))
+    end
+
+    # Ron - what if no observation???
+    #generate posteriors 
+    # τb = [b0]
+    # for i in 1:T-1
+    #     push!(τb, PropagateUpdateBelief(τb[end],  𝒫, ak, τobsbeacons[i+1])) 
+    # end
+    
+    # plots 
+    dr2=scatter([x[1] for x in τ], [x[2] for x in τ], label="gt")
+    for i in 1:T
+        covellipse!(τbp[i].μ, τbp[i].Σ, showaxes=true, n_std=3, label="step $i")
+    end
+    # savefig([dr2, bplot], "dr2.pdf") Ron -how to combine two scatters???
+    savefig(dr2, "dr2.pdf")
+    
+    # Ron - what if no observation???
+    # tr2=scatter([x[1] for x in τ], [x[2] for x in τ], label="gt")
+    # for i in 1:T
+    #     covellipse!(τb[i].μ, τb[i].Σ, showaxes=true, n_std=3, label="step $i")
+    # end
+    # savefig(tr2,"tr.pdf")
+
+end
+
+function main()
+    # part1()
+    part2()
     
     # use function det(b.Σ) to calculate determinant of the matrix
 end 
