cc9ff92a446472564747cc0d2f6e6e945d8296e1
orrLani <olaniado@campus.technion.ac.il>
2022-05-02 01:21:46 +0300

    wip plot nodes,edges, obstacle

--- hw3/PRM.py
+++ hw3/PRM.py
@@ -1,5 +1,6 @@
 # assume all the polygons on the same size
 import random
+import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.patches import Rectangle
@@ -9,10 +10,12 @@ X_LIMIT_RIGHT = 300
 
 Y_LIMIT_DOWN = 0
 Y_LIMIT_UP = 300
-N = 100
+N = 10
 X_Obs = 15
-Y_Obs =  10
-def is_legal(x,y) ->bool:
+Y_Obs = 10
+
+
+def is_legal(x,y) -> bool:
     if x < X_LIMIT_LEFT:
         return False
     if x + X_Obs > X_LIMIT_RIGHT:
@@ -36,13 +39,10 @@ class Node(object):
     def __init__(self, x_pos, y_pos):
         self.x_pos = x_pos
         self.y_pos = y_pos
-        self.near_node = None
+        # self.near_node = None
 
 
-    def __eq__(self,other):
-        if self.x_pos == other.x_pos and self.y_pos == other.y_pos:
-            return True
-        return False
+
 
     def __str__(self):
         return f'{self.x_pos}_{self.y_pos}'
@@ -67,18 +67,81 @@ class PRM(object):
         plt.ylim([Y_LIMIT_DOWN, Y_LIMIT_UP])
         plt.show()
 
+    def plot_nodes(self):
+        fig, ax = plt.subplots()
+        # add obstacles
+        print(self.forest)
+        #     ax.add_patch(Rectangle((obstacle.x_left, obstacle.y_left), X_Obs, Y_Obs,
+        #                            edgecolor='blue', lw=1, fill=True, facecolor='red'))
+        #
+        # plt.xlim([X_LIMIT_LEFT, X_LIMIT_RIGHT])
+        # plt.ylim([Y_LIMIT_DOWN, Y_LIMIT_UP])
+        # plt.show()
+        X_list = [float(key.split('_')[0]) for key in self.forest.keys()]
+        Y_list = [float(key.split('_')[1]) for key in self.forest.keys()]
+
+        plt.scatter(X_list, Y_list, marker="o")
+
+    def plot_nodes_with_edges(self):
+        plt.figure(figsize=(36, 36))
+        G = nx.Graph()
+        # add nodes
+        [G.add_node(key, pos=(float(key.split('_')[0]), float(key.split('_')[1]))) for key in self.forest.keys()]
+        # add edges
+        [[G.add_edge(key, self.forest[key][i]) for i in range(0, len(self.forest[key]))]
+                    for key in self.forest.keys()]
+
+
+        pos = nx.get_node_attributes(G, 'pos')
+        nx.draw(G, pos)
+        plt.show()
+
+    def plot_all(self):
+        f, ax = plt.subplots(1, 1, figsize=(28,28))
+
+        G = nx.Graph()
+        # add nodes
+        [G.add_node(key, pos=(float(key.split('_')[0]), float(key.split('_')[1]))) for key in self.forest.keys()]
+        # add edges
+        [[G.add_edge(key, self.forest[key][i]) for i in range(0, len(self.forest[key]))]
+                    for key in self.forest.keys()]
+
+        pos = nx.get_node_attributes(G, 'pos')
+        nx.draw(G, pos)
+
+        # add obstacles
+        for obstacle in self.obstacles_list:
+            ax.add_patch(Rectangle((obstacle.x_left, obstacle.y_left), X_Obs, Y_Obs,
+                                   edgecolor='blue', lw=1, fill=True, facecolor='red'))
+        plt.show()
+
+
+
     def add_node(self,node:Node)->bool:
         # check if the node not in "bad" area
         for obstacle in self.obstacles_list:
-            if (obstacle.x_left<=node.x_pos<=obstacle.x_right and
+            if (obstacle.x_left <= node.x_pos <= obstacle.x_right and
                     obstacle.y_left <= node.y_pos <= obstacle.y_right):
                 return False
 
         # add node to the node-list
+        if not str(node) in self.forest.keys():
+            self.forest[str(node)] = []
+
         nearest_neighbor = self._nearest_neighbors(node)
 
-        return True
+        # nearest not dont exist
+        if nearest_neighbor is None:
+            return True
 
+        # add node
+        self.forest[str(node)].append(str(nearest_neighbor))
+
+        if not str(nearest_neighbor) in self.forest[str(nearest_neighbor)]:
+            self.forest[str(nearest_neighbor)] = []
+        self.forest[str(nearest_neighbor)].append(str(node))
+
+        return True
 
     def _nearest_neighbors(self, node:Node) -> Node:
 
@@ -87,18 +150,14 @@ class PRM(object):
         min_distance = np.Inf
         nearest_neighbor = None
         for neighbor in self.forest.keys():
-            distance_x = (node.x_pos - neighbor.x_pos)**2
-            distance_y = (node.y_pos - neighbor.y_pos)**2
+            distance_x = (node.x_pos - float(neighbor.split('_')[0]))**2
+            distance_y = (node.y_pos - float(neighbor.split('_')[1]))**2
             distance = (distance_x + distance_y)**0.5
-            if distance < min_distance:
+            if distance < min_distance and distance != 0 and distance< self.thd:
                 min_distance = distance
                 nearest_neighbor = neighbor
 
-
-
-    def plot_nodes(self):
-
-        pass
+        return nearest_neighbor
 
 def GeneratePRM(thd:float,nodes:int,obstacles_list:list):
 
@@ -117,8 +176,8 @@ def GeneratePRM(thd:float,nodes:int,obstacles_list:list):
         if num_nodes_added >= prm_model.nodes_number:
             break
 
-    prm_model.plot_obstacles()
-
+    # prm_model.plot_obstacles()
+    prm_model.plot_all()
 
 def draw_configurations():
     obstacles_list = list()
