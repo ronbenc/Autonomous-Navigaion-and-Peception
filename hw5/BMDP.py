ac0b2af85876a36ba307bbf581ee252096461bb1
Ron Benchetrit <ronbenc@campus.technion.ac.il>
2022-05-17 20:40:58 +0300

    Create BMDP planner

--- hw5/BMDP.py
+++ hw5/BMDP.py
@@ -1,15 +1,18 @@
+from msilib.schema import Class
 from tkinter.messagebox import NO
 import numpy as np
 
 class BMDPscenaraio:
-    def __init__(self, belief_mean, belief_cov, F, process_cov, beacons, d, rmin):
+    def __init__(self, belief_mean, belief_cov, F, process_cov, beacons, actions, d, rmin, lambda_reg):
         self.belief_mean = belief_mean
         self.belief_cov = belief_cov
         self.F = F
         self.process_cov = process_cov
         self.beacons = beacons # 2XN
+        self.actions = actions
         self.d = d
         self.rmin=rmin
+        self.lambda_reg = lambda_reg
         self.last_obs_cov = None
 
 
@@ -46,3 +49,30 @@ class BMDPscenaraio:
         obs = self._generate_observation_from_beacons(sampled_state)
         if obs is not None:
             self._update_belief(obs)
+    
+
+    def _cost(self, goal_state):
+        cost = np.linalg.norm(self.belief_mean - goal_state) + lambda_reg*np.linalg.det(self.belief_cov)
+        return cost
+
+
+class BMPD_planner:
+    def __init__(self, goal_state, number_of_samples) -> None:
+        self.goal_state = goal_state
+        self.number_of_samples = number_of_samples
+
+    
+    def sparse_sampling(self, goal_state, number_of_samples, horizon):
+        if horizon <= 0:
+            return None, None
+        best_action, best_value = None, -np.inf
+        curr_belief_mean, curr_belief_cov = self.belief_mean, self.belief_cov
+        for action in self.actions:
+            q = 0
+            for i in range(number_of_samples):
+                self.transit_belief_MDP(action)
+                cost = self._cost(goal_state)
+                selected_action, value = self.sparse_sampling(goal_state, number_of_samples, horizon-1)
+
+
+
--- hw5/main.py
+++ hw5/main.py
@@ -9,21 +9,24 @@ if __name__ == '__main__':
     initial_belief_cov = np.eye(2, dtype=float)
     process_cov = 0.01 * np.eye(2, dtype=float)
     initial_gt = np.array([-0.5, -0.2], dtype=float)
-
-    bmdp = BMDPscenaraio(initial_belief_mean, initial_belief_cov, np.eye(2), process_cov, beacons, d=1, rmin=0.1)
+    actions = np.array([[1, 0], [-1, 0], [0, 1], [0, -1], 
+                    [1/np.sqrt(2), 1/np.sqrt(2)], [-1/np.sqrt(2), 1/np.sqrt(2)], [1/np.sqrt(2), -1/np.sqrt(2)], [-1/np.sqrt(2), -1/np.sqrt(2)]], dtype=float)
+    bmdp = BMDPscenaraio(initial_belief_mean, initial_belief_cov, np.eye(2), process_cov, beacons, actions, d=1, rmin=0.1)
+    x_0_gt = np.array([-0.5, -0.2])
 
     # test
-    T = 10
+    T = 25
     action = np.array([0.5, 0.5])
 
-    belief_means_x = []
-    belief_means_y = []
-    belief_covs = []
+    belief_means_x = [initial_belief_mean[0]]
+    belief_means_y = [initial_belief_mean[1]]
+    belief_covs = [initial_belief_cov]
     for i in range(T):
         bmdp.transit_belief_MDP(action)
         belief_means_x.append(bmdp.belief_mean[0])
         belief_means_y.append(bmdp.belief_mean[1])
         belief_covs.append(bmdp.belief_cov)
 
+    plt.scatter(beacons[:, 0], beacons[:, 1])
     plt.scatter(belief_means_x, belief_means_y)
     plt.show()
